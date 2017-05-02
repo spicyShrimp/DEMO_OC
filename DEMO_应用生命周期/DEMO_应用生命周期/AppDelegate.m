a62cf52a0bea4eaea68441859398031dc75ab456
charles <78268731@qq.com>
2017-05-02 10:29:22 +0800

    DEMO_应用生命周期

--- DEMO_应用生命周期/DEMO_应用生命周期/AppDelegate.m
+++ DEMO_应用生命周期/DEMO_应用生命周期/AppDelegate.m
@@ -15,35 +15,50 @@
 @implementation AppDelegate
 
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+-(BOOL)application:(UIApplication *)application willFinishLaunchingWithOptions:(NSDictionary *)launchOptions{
+    NSLog(@"%s",__FUNCTION__);
     return YES;
 }
 
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // Override point for customization after application launch.
+    NSLog(@"%s",__FUNCTION__);
+    return YES;
+}
+
+-(void)applicationDidFinishLaunching:(UIApplication *)application{
+    NSLog(@"%s",__FUNCTION__);
+    
+}
 
 - (void)applicationWillResignActive:(UIApplication *)application {
+    NSLog(@"%s",__FUNCTION__);
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
 }
 
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
+    NSLog(@"%s",__FUNCTION__);
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 }
 
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
+    NSLog(@"%s",__FUNCTION__);
     // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
 }
 
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
+    NSLog(@"%s",__FUNCTION__);
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
 }
 
 
 - (void)applicationWillTerminate:(UIApplication *)application {
+    NSLog(@"%s",__FUNCTION__);
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
